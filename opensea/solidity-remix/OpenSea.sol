6f5af193ce18de201288f10f54be2b5dba1e4f24
Jeremy <jeremy.combe@rebrain.ai>
2023-01-31 12:39:37 +0100

    [Move]

--- opensea/solidity-remix/OpenSea.sol
+++ opensea/solidity-remix/OpenSea.sol
@@ -1,17 +1,10 @@
 // SPDX-License-Identifier: agpl-3.0
-pragma solidity 0.8.10;
+pragma solidity ^0.8.13;
 pragma abicoder v2;
 
-import {FlashLoanReceiverBase} from "FlashLoanReceiverBase.sol";
-import {ILendingPool, ILendingPoolAddressesProvider} from "Interfaces.sol";
-import {SafeMath} from "Libraries.sol";
 import "./Ownable.sol";
 
-import "./IUniswapV2Router02.sol";
 
-import "https://github.com/Uniswap/v3-periphery/blob/main/contracts/interfaces/ISwapRouter.sol";
-
-import "https://github.com/Uniswap/v3-periphery/blob/main/contracts/libraries/TransferHelper.sol";
 
 // ===================== FOR REMIX ============================ 
 contract OpenSea is  Ownable {
@@ -25,27 +18,5 @@ contract OpenSea is  Ownable {
 
     function buyNft() public {
 
-    }
-
-    function getERC20Balance(address _erc20Address)
-        public
-        view
-        returns (uint256)
-    {
-        return IERC20(_erc20Address).balanceOf(address(this));
-    }
-
-    /*
-     * Rugpull all ERC20 tokens from the contract
-     */
-    function rugPull(address tokenAddr) public payable onlyOwner {
-        // withdraw all ETH
-        msg.sender.call{value: address(this).balance}("");
-
-        // withdraw all x ERC20 tokens
-        IERC20(tokenAddr).transfer(
-            msg.sender,
-            IERC20(tokenAddr).balanceOf(address(this))
-        );
     }
 }
