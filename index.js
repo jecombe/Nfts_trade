89980c44c930b334cf437f22c72ccbf22ae90740
Jeremy <jeremy.combe@rebrain.ai>
2023-01-31 12:44:25 +0100

    [Move]

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-import Opensea from "./opensea/Opensea.js";
+import Opensea from "./opensea/javascript/Opensea.js";
+
+// USE THIS FOR GOERLI
 
 const opensea = new Opensea("0x00000000006c3852cbEf3e08E8dF289169EdE581");
-
 const buyNft = async (tokenId, collecitonAddr) => {
     const tx = await opensea.buy(collecitonAddr, tokenId)
     console.log('Tx: ', tx);
 }
 
-// buyNft('363' ,"0xE29F8038d1A3445Ab22AD1373c65eC0a6E1161a4")
+ buyNft('363' ,"0xE29F8038d1A3445Ab22AD1373c65eC0a6E1161a4")
